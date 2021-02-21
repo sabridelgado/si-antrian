442a9997efbe67b910d0143b8d8e890263793a66
sabridelgado <sabrigithub@gmail.com>
2021-02-21 15:34:44 +0800

    test

--- application/views/user/home.php
+++ application/views/user/home.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Welcome To My Company</title>
+    <title>Welcome To My Company Profile</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
