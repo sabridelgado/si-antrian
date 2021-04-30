9b3636fa44dd347fda2143a31c16660c3b4bdfcb
sabridelgado <sabrigithub@gmail.com>
2021-04-30 23:48:15 +0800

    WEB Antrian

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -21,6 +21,35 @@ class Home extends CI_Controller
 	 */
 	public function index()
 	{
+
+		$tempat = "assets/img/";
+		$kodenya = 'http://192.168.1.9/antriancapil/index.php/home/order';
+
+		QRcode::png(
+			$kodenya,
+			$tempat . "qrcode01.png",
+			QR_ECLEVEL_L,
+			100,
+			4
+		);
 		$this->load->view('user/home');
+	}
+
+	public function order()
+	{
+		$this->load->view('user/order');
+	}
+	public function scanqr()
+	{
+		$tempat = "assets/img/";
+		$kodenya = 'https://www.youtube.com/';
+
+		QRcode::png(
+			$kodenya,
+			$tempat . "qrcode01.png",
+			QR_ECLEVEL_L,
+			10,
+			4
+		);
 	}
 }
